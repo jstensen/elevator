48b504c26ccb85d474910abe72e61f8f355c4bff
Julianne Stensen <julianne.stensen@vy.no>
2022-01-23 20:48:55 +0100

    La til funksjon som returnerer estimert reisetid fra etasje X til Y.

--- src/main/kotlin/elevator/Elevator.kt
+++ src/main/kotlin/elevator/Elevator.kt
@@ -1,5 +1,7 @@
 package elevator
 
+import java.lang.IllegalArgumentException
+
 class Elevator(
     val numberOfFloors: Int,
     var currentFloor: Int = 0,
@@ -7,8 +9,31 @@ class Elevator(
     var speedPerFloor: Float = 1.0f,
 ) {
     fun moveTo(destinationFloor: Int) {
+        println("New move command from the elevator controller to the elevator.")
+        println("---------------------------------------------")
+        println("Current floor: $currentFloor")
+        println("Going to floor: $destinationFloor")
+        state = if (destinationFloor > currentFloor) {
+            ElevatorState.GOING_UP
+        } else {
+            ElevatorState.GOING_DOWN
+        }
+        println("Elevator state: $state")
+
         currentFloor = destinationFloor
+        println("Arrived at destination floor: $destinationFloor")
+        state = ElevatorState.IDLE
+        println("Elevator state: $state")
     }
+
+    fun estimatedTimeToFloor(destinationFloor: Int) : Float {
+        if (destinationFloor > numberOfFloors) {
+            throw IllegalArgumentException("Destination floor greater than elevator size")
+        }
+
+        return (destinationFloor - currentFloor) * speedPerFloor
+    }
+
 }
 
 enum class ElevatorState {
--- src/test/kotlin/elevator/ElevatorControllerTest.kt
+++ src/test/kotlin/elevator/ElevatorControllerTest.kt
@@ -94,5 +94,13 @@ internal class ElevatorControllerTest {
 
     }
 
+    @Test
+    fun `Test get estimated elevator travel time when travelling from 1st to 4th floor`() {
+        val elevator = Elevator(numberOfFloors = 10, currentFloor = 1)
+        val estimatedTimeFrom1stTo4thFloor = elevator.estimatedTimeToFloor(destinationFloor = 4)
+
+        assertEquals(expected = (3 * elevator.speedPerFloor), actual = estimatedTimeFrom1stTo4thFloor)
+    }
+
 
 }
